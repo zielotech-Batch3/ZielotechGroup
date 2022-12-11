b5215232272ed882cd7e0eacaf2e28492d8f219c
niharpaitech <nihar.paitech@gmail.com>
2022-12-11 02:18:36 +0000

    2nd stash

--- Product.java
+++ Product.java
@@ -5,11 +5,12 @@ public class HelloWorld {
         // Creates a reader instance which takes
         // input from standard input - keyboard
         Scanner reader = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a valid number: ");
         // nextInt() reads the next integer from the keyboard
         int number = reader.nextInt();
         // println() prints the following line to the output screen
         System.out.println("You entered: " + number);
 	// This is a comment line for demonstrating git stash
+        System.out.println("You have entered the number : " + number);
     }
 }
